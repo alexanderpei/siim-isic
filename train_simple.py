893337caa69ed314b7ee29d81c69903f9d449ef5
alexanderpei <apei2@andrew.cmu.edu>
2020-06-13 16:38:50 -0400

    simple example added, made train_tta.py

--- train_simple.py
+++ train_simple.py
@@ -5,8 +5,8 @@ import tensorflow as tf
 
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.optimizers import Adam, SGD
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.losses import BinaryCrossentropy
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.layers import Activation, Dropout, Flatten, Dense, Conv2D, MaxPooling2D, GlobalAveragePooling2D
 
 # Check if on google colab. If not, set the base directory to the current one.
@@ -23,7 +23,7 @@ nEpochs = 3     # Number of training epochs.
 lr = 0.0005     # Learning rate
 foldNum = 0     # K-fold number you want to use (0-4)
 
-# ------------------ Creating model ------------------ #
+# --------------------- Creating model ---------------------- #
 
 # Choose your optimizer function. For example, try SGD
 opt = Adam(lr=lr)
@@ -55,17 +55,20 @@ model.compile(
 # memorizing the images. We create a new test generator since we don't want to change around the test images.
 
 trainGen = ImageDataGenerator(
-            rescale=1./225,         # Scale down the image which should improve learning gradients
-            rotation_range=180,     # Randomly rotate the image from -180 to 180 degrees
-            width_shift_range=0.2,  # Randomly shift the width of the image
-            height_shift_range=0.2, # "
-            horizontal_flip=True,   # Randomly flip the image horizontally
-            vertical_flip=True,     # "
-            fill_mode='nearest')    # Fill Nan values with the nearest pixel
+            rescale=1./225,             # Scale down the image which should improve learning gradients
+            rotation_range=180,         # Randomly rotate the image from -180 to 180 degrees
+            width_shift_range=0.2,      # Randomly shift the width of the image
+            height_shift_range=0.2,     # "
+            horizontal_flip=True,       # Randomly flip the image horizontally
+            vertical_flip=True,         # "
+            fill_mode='nearest')        # Fill Nan values with the nearest pixel
 
 testGen = ImageDataGenerator(
             rescale=1./225,
             fill_mode='nearest')
+
+# Now that we've created our image data generators, we need to specify where to load the images from.
+# The flow_from_directory() method does exactly this.
 
 trainIm = trainGen.flow_from_directory(
             os.path.join(pathBase, 'data' + str(foldNum), 'train'),
@@ -88,7 +91,9 @@ testIm = testGen.flow_from_directory(
 
 # ------------------ Train the model ------------------ #
 
-# Need to specify how many batches we want to use during training
+# Need to specify how many batches we want to use during training. The method below will make sure that each image
+# gets passed through every epoch. If your computer is slow, you can change float(len(trainIm.filenames)) to
+# something smaller than the total number of images (roughly 40,000, try changing to 2000 for speed).
 steps_per_epoch = np.ceil(float(len(trainIm.filenames)) / float(batchSize))
 validation_steps = np.ceil(float(len(valIm.filenames)) / float(batchSize))
 
@@ -104,6 +109,7 @@ model.fit(
 
 print('Generating predictions and submission file...')
 
+# Creating a pandas data frame with the names of all of the images
 df_test = pd.DataFrame({
     'image_name': os.listdir(os.path.join(os.getcwd(), '512x512-test', '512x512-test'))
 })
